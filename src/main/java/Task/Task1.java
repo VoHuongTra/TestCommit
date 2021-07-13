35ec4f7b52fdec8126411d4253d8af8c168f1661
hangdm3 <hangdm3@vingroup.net>
2021-07-13 12:17:24 +0700

    sample test

---
+++ src/main/java/Task/Task1.java
@@ -0,0 +1,29 @@
+package Task;
+
+/**
+ * Write a program that reads a string from the keyboard.
+ * The program should display two lines:
+ * 1. The first line contains only the vowels from the entered string.
+ * 2. The second contains only the consonants and punctuation marks from the entered string.
+ * The letters should be separated by a space, each line must end with a space.
+ *
+ * Example input:
+ * Sam I Am.
+ *
+ * Example output:
+ * a I A
+ * S m m .
+ */
+public class Task1 {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+
+    }
+
+
+    public boolean isVowel(char c) {
+        // write your code here
+        return true;
+    }
+}
---
+++ src/main/java/Task/Task2.java
@@ -0,0 +1,28 @@
+package Task;
+
+/**
+ * Enter 20 words from the keyboard and display them in alphabetical order. Each word should be on a new line.
+ *
+ * Requirements:
+ * •	The program should display text on the screen.
+ * •	The program should read values from the keyboard.
+ * •	The Solution class must have three methods.
+ * •	The main() method should call the sort() method.
+ * •	The sort () method should call the isGreaterThan() method.
+ * •	The displayed words should be sorted alphabetically.
+ */
+public class Task2 {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+
+    public void sort(String[] array) {
+        //write your code here
+
+    }
+
+    // String comparison method: 'a' is greater than 'b'
+    public boolean isGreaterThan(String a, String b) {
+        return true;
+    }
+}
